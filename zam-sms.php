d6241467994734b1833892b1171c6abfef7e959c
kazashim <kazashimkuzasuwat@gmail.com>
2020-04-27 15:08:39 +0200

    1

--- zam-sms.php
+++ zam-sms.php
@@ -62,5 +62,54 @@ public static function Instantiate() {
 						1
 					);
 					
-			}
-}
+            }
+            private function MainPageHTML() {
+				add_filter('admin_footer_text', function() {	// Add a footer that links to our website
+					$this->AddAdminFooter();
+				});
+				
+				$send_results_html = '';
+				$send_results_msg_bubble = '';
+				if (isset($_POST['send_sms'])) {
+					$send_results = $this->SendSMS();
+					
+					//$send_results_html = '
+					//	<pre>
+					//		'.print_r($send_results, true).'
+					//	</pre>
+					//';
+					
+					$status = isset($send_results['status']) ? sanitize_text_field($send_results['status']) : '';
+					
+					if ($status == 'queued') {
+						
+						$contacts = isset($send_results['contacts']) ? sanitize_text_field($send_results['contacts']) : '';
+						$senderid = isset($send_results['senderid']) ? sanitize_text_field($send_results['senderid']) : '';
+						$message = isset($send_results['message']) ? sanitize_text_field($send_results['message']) : '';
+					
+						
+						$send_results_html .= '
+							<div class="notice notice-success is-dismissible">
+								<p>Your message has been queued for delivery.</p>
+							</div>
+						';
+						
+						$send_results_msg_bubble .= '<div class="msg-bubble">';
+							$send_results_msg_bubble .= '<div style="margin-bottom:5px;">';
+								$send_results_msg_bubble .= '<b style="font-size:0.8em; font-weight:700;">Sent</b>';
+								$send_results_msg_bubble .= '<b style="float:right; font-size:0.8em; font-weight:700;">'.$to.'</b>';
+							$send_results_msg_bubble .= '</div>';
+							$send_results_msg_bubble .= $body;
+						$send_results_msg_bubble .= '</div>';
+					}
+					else {
+						$send_results_html .= '
+							<div class="notice notice-error is-dismissible">
+								<p>There was an error sending your message.</p>
+							</div>
+							<div class="notice notice-error is-dismissible">
+								<p>Please check and confirm that your settings are correct and that your recipient\'s phone number was entered correctly.</p>
+							</div>
+						';
+					}
+				}
