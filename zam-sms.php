d8f0b52dbd03fce8ce1681629efcdfc700e304ff
kazashim <kazashimkuzasuwat@gmail.com>
2020-04-27 14:55:12 +0200

    first

---
+++ zam-sms.php
@@ -0,0 +1,16 @@
+<?php
+	/*
+		Plugin Name: ZamtelSMS
+		Description: Use ZamtalBulkSMS to send SMS from your website!
+		Version: 1.0.0
+		Author: Cynojine Technologu
+    */
+    
+    if (!defined('ABSPATH')) {
+		exit; // Exit if accessed directly
+    }
+    
+
+    class ZamSMS {
+
+    }
